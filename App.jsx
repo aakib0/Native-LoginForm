4100328cafee14e91964f2d33e3ef06a8db9bae5
Your Name <your@email.com>
2025-10-23 10:07:39 +0530

    Added or updated Login Form files

--- App.jsx
+++ App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-gray-100">
       <Text className="text-3xl font-bold text-blue-600 mb-4">
-        Hello Tailwind 👋
+        Hello Tailwind 
       </Text>
       <Pressable className="bg-blue-500 px-4 py-2 rounded-lg">
         <Text className="text-white text-lg">Click Me</Text>
